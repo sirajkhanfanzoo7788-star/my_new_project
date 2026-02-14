6e96b9b5171a0fd0111e0d938778ba2b23eb4f65
Siraj Ahmad <sirajkhanfanzoo7788@gmail.com>
2026-02-14 20:19:21 +0500

    auto update check add github action workflow new update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World! This is my Node.js app ALHUMDULILLAH TO DEPLOY .\n');
+  res.end('Hello World! This is my Node ALHUMDULILLAH  .\n');
 });
 
 server.listen(PORT, () => {
